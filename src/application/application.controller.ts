2a8aab651fe0da8b4dd90f88232ea288ba515d6e
vyantik <akarizidu2@gmail.com>
2025-05-02 00:52:50 +0300

    feat(application): add authorization for application creation
    Added @Authorization decorator to the ApplicationController to restrict access to the application creation endpoint, allowing only users with the ADMIN role to create applications. This enhances security and role management within the application.

--- src/application/application.controller.ts
+++ src/application/application.controller.ts
@@ -1,5 +1,8 @@
 import { Body, Controller, Post } from '@nestjs/common'
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
+import { UserRole } from '@prisma/__generated__'
+
+import { Authorization } from '@/auth/decorators/auth.decorator'
 
 import { ApplicationService } from './application.service'
 import { ApplicationDto } from './dto/application.dto'
@@ -10,6 +13,7 @@ export class ApplicationController {
 	constructor(private readonly applicationService: ApplicationService) {}
 
 	@Post()
+	@Authorization(UserRole.ADMIN)
 	@ApiOperation({ summary: 'Создание заявки' })
 	@ApiResponse({ status: 201, description: 'Заявка успешно создана' })
 	@ApiBody({ type: ApplicationDto })
